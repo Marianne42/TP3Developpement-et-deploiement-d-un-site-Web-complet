73ad3f2f08c5409a2f6e5b93fcd47a798890fbda
marianne24 <2138283@cmontmorency.qc.ca>
2023-05-20 13:28:45 -0400

    modification schema etudiant

--- models/etudiants.js
+++ models/etudiants.js
@@ -4,8 +4,8 @@ const etudiantsSchema = new mongoose.Schema({
     NumDa: {type: Number, required: true},
     nom: {type: String, required: true},
     courriel: {type: String, required: true},
-    profileSortie: {type: String, required: true},
-        
+    profilSortie: {type: String, required: true},
+    idStage: {type: mongoose.Types.ObjectId, required: true}    
 });
 
 module.exports = mongoose.model("etudiants", etudiantsSchema);
