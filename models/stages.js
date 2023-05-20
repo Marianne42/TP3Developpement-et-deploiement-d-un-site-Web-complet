e0423208adec0e5c2f694d637ece6d651d186430
marianne24 <2138283@cmontmorency.qc.ca>
2023-05-20 14:56:56 -0400

    ajout module mongoose

--- models/stages.js
+++ models/stages.js
@@ -6,10 +6,11 @@ const stageSchema = new mongoose.Schema({
     numTelephone: {type: Number, required: true},
     nomEntreprise: {type: String, required: true},
     adresseEntreprise: {type: String, required: true},
-    TypeStage: {type: String, required: true},
+    TypeStage: {type: String, enum: ['Réseaux et sécurité', 'Développement dapplications'], required: true},
     nbrPosteDispo: {type: Number, required: true},
     Description: {type: String, required: true},
-    remuneration: {type: String, required: true}
+    remuneration: {type: String, enum: ['salaire horaire', 'montant unique', 'aucune rémunération'], required: true},
+    listeEtudiant: [{type: mongoose.Types.ObjectId, required: false, ref:"etudiants"}]
 });
 
 module.exports = mongoose.model("stages", stageSchema);
